7fbfa0e883b1fd2bde1b5bc7d798f163aca368df
Nicolas Payrouse <nicolas.payrouse@smood.ch>
2026-03-16 10:43:05 +0100

    Push lab app

--- libs/wui/src/components/WuiTooltip/WuiTooltip.props.ts
+++ libs/wui/src/components/WuiTooltip/WuiTooltip.props.ts
@@ -7,7 +7,8 @@ export enum WuiTooltipPlacement {
   RIGHT = "right",
 }
 
-export interface WuiTooltipProps extends HTMLAttributes<HTMLSpanElement> {
+export interface WuiTooltipProps
+  extends Omit<HTMLAttributes<HTMLSpanElement>, "content"> {
   /** Content shown in the tooltip when hovering the trigger. */
   content: ReactNode;
   /** Preferred placement relative to the trigger. Uses CSS anchor positioning. */
