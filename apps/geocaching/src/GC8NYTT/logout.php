6961641d8818d1a501eafa205e927219d3c536cc
Nicolas Payrouse <nicolas.payrouse@smood.ch>
2026-03-17 22:33:42 +0100

    remove geocaching src

--- apps/geocaching/src/GC8NYTT/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-unset($_COOKIE['GC8NYTT']);
-setcookie("GC8NYTT", '', time() - 3600, '/');
-header('Location: ./index.php');
-exit();
-?>
--- apps/geocaching/src/NEWS/reviewer-d18ad151-22b3-45e9-8da7-900c77e56739/inc/globals.php
+++
@@ -1,6 +0,0 @@
-<?php
-$SERVER = 	"mysql51-56.perso";
-$USER = 	"wawawoom123";
-$PASSWORD = "4HZIpSO7";
-$DATABASE = "wawawoom123";
-?>
